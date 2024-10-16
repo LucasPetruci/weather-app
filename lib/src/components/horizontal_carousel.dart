64da99d737be043c053f475a18d40f9f8af9ad79
LucasPetruci <lucaspetruci100@gmail.com>
2024-10-16 16:47:20 -0300

    feat: add carousel for weather details

---
+++ lib/src/components/horizontal_carousel.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/src/components/boxDetails.dart';
+
+class HorizontalCarousel extends StatelessWidget {
+  final String precipitation, wind, cloudCover;
+  const HorizontalCarousel(
+      {super.key,
+      required this.precipitation,
+      required this.wind,
+      required this.cloudCover});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        children: [
+          MyBoxDetails(
+            path: 'assets/box/precipitation.png',
+            content: precipitation,
+            text: "Preciptação",
+          ),
+          MyBoxDetails(
+            path: 'assets/box/wind.png',
+            content: wind,
+            text: "Vento",
+          ),
+          MyBoxDetails(
+            path: 'assets/box/nuvem.png',
+            content: cloudCover,
+            text: "Cobertura de nuvem",
+          ),
+        ],
+      ),
+    );
+  }
+}
