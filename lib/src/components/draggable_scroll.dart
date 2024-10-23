5c48d1f0c93ced7c266ff4a42a229613a800b1a4
LucasPetruci <lucaspetruci100@gmail.com>
2024-10-23 00:15:37 -0300

    Refactor: Update draggable scrollable sheet component to use days of week

--- lib/src/components/draggable_scroll.dart
+++ lib/src/components/draggable_scroll.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class MyDraggableScrollable extends StatelessWidget {
   const MyDraggableScrollable({super.key});
@@ -7,6 +8,19 @@ class MyDraggableScrollable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String _imgPath = 'assets/icons/1.png';
+    DateTime parsedDate = DateFormat("dd/MM").parse("19/09");
+
+    //list array days of the week
+    List<String> days = [
+      'Domingo',
+      'Segunda',
+      'Terça',
+      'Quarta',
+      'Quinta',
+      'Sexta',
+      'Sábado'
+    ];
+    String dayOfWeek = days[parsedDate.weekday - 1];
     return Expanded(
       child: DraggableScrollableSheet(
         initialChildSize: 0.3,
@@ -28,33 +42,42 @@ class MyDraggableScrollable extends StatelessWidget {
                 ),
                 SingleChildScrollView(
                   controller: scrollController,
-                  child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            "19/09",
-                            style: GoogleFonts.lato(
-                              fontSize: 20,
-                              color: Colors.blue,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      // Aqui está a mudança para incluir o Divider
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              dayOfWeek,
+                              style: GoogleFonts.lato(
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
                             ),
-                          ),
-                          Spacer(),
-                          Image.asset(
-                            _imgPath,
-                            width: 50,
-                            height: 50,
-                          ),
-                          SizedBox(width: 20),
-                          Text(
-                            '22°C',
-                            style: GoogleFonts.lato(
-                                fontSize: 22, color: Colors.blue),
-                          ),
-                          SizedBox(width: 20),
-                        ],
-                      ),
+                            Spacer(),
+                            Image.asset(
+                              _imgPath,
+                              width: 50,
+                              height: 50,
+                            ),
+                            SizedBox(width: 20),
+                            Text(
+                              '22°C',
+                              style: GoogleFonts.lato(
+                                fontSize: 22,
+                                color: Colors.blue,
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                          ],
+                        ),
+                        Divider(
+                          thickness: 1,
+                          color: Color.fromRGBO(231, 231, 231, 1),
+                        ),
+                      ],
                     ),
                   ),
                 ),
