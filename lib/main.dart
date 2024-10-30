e577e123e00c49e81aed7306011715346304f49c
LucasPetruci <lucaspetruci100@gmail.com>
2024-10-29 22:30:16 -0300

    Refactor: Update main.dart to initialize date formatting for pt_BR locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart'; // Import correto
 import 'package:provider/provider.dart';
 import 'package:weather_app/src/pages/weather_page.dart';
 import 'package:weather_app/src/services/weather_api_service.dart';
@@ -6,7 +7,10 @@ import 'package:weather_app/src/services/weather_api_service.dart';
 import 'src/components/scroll_behavior_mofied.dart';
 import 'src/controllers/weather_controller.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR'); // Remova o null
+
   runApp(
     MultiProvider(
       providers: [
