c132d0ca30e6593f1c808f0a93dc39d7500a4042
LucasPetruci <lucaspetruci100@gmail.com>
2024-10-29 22:31:19 -0300

    Refactor: Update MyDraggableScrollable to include forecast data and convert date to day of the week

--- lib/src/components/draggable_scroll.dart
+++ lib/src/components/draggable_scroll.dart
@@ -3,34 +3,26 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 class MyDraggableScrollable extends StatelessWidget {
-  const MyDraggableScrollable({super.key});
+  final List<Map<String, dynamic>> forecastData;
+
+  const MyDraggableScrollable({
+    super.key,
+    required this.forecastData,
+  });
 
   @override
   Widget build(BuildContext context) {
-    String _imgPath = 'assets/icons/1.png';
-    DateTime parsedDate = DateFormat("dd/MM").parse("19/09");
-
-    //list array days of the week
-    List<String> days = [
-      'Domingo',
-      'Segunda',
-      'Terça',
-      'Quarta',
-      'Quinta',
-      'Sexta',
-      'Sábado'
-    ];
-    String dayOfWeek = days[parsedDate.weekday - 1];
     return Expanded(
       child: DraggableScrollableSheet(
         initialChildSize: 0.3,
         minChildSize: 0.2,
-        maxChildSize: 0.8,
+        maxChildSize: 1.0,
         builder: (context, scrollController) {
           return ColoredBox(
             color: Colors.white,
             child: Column(
               children: [
+                // Handle do Draggable Sheet
                 Container(
                   margin: const EdgeInsets.only(top: 10),
                   width: 50,
@@ -40,45 +32,56 @@ class MyDraggableScrollable extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                SingleChildScrollView(
-                  controller: scrollController,
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      // Aqui está a mudança para incluir o Divider
-                      children: [
-                        Row(
+                // Lista de previsões
+                Expanded(
+                  child: ListView.builder(
+                    controller: scrollController,
+                    itemCount: forecastData.length,
+                    itemBuilder: (context, index) {
+                      final item = forecastData[index];
+
+                      // converter para od dias da semana
+                      String dayOfWeek = _convertToDayOfWeek(item['dayOfWeek']);
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
                           children: [
-                            Text(
-                              dayOfWeek,
-                              style: GoogleFonts.lato(
-                                fontSize: 20,
-                                color: Colors.blue,
-                              ),
+                            Row(
+                              children: [
+                                Text(
+                                  dayOfWeek,
+                                  style: GoogleFonts.lato(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                                const Spacer(),
+                                Image.asset(
+                                  item['imgPath'],
+                                  width: 50,
+                                  height: 50,
+                                ),
+                                const SizedBox(width: 20),
+                                Text(
+                                  '${item['temperature']}°C',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 22,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                                const SizedBox(width: 20),
+                              ],
                             ),
-                            Spacer(),
-                            Image.asset(
-                              _imgPath,
-                              width: 50,
-                              height: 50,
-                            ),
-                            SizedBox(width: 20),
-                            Text(
-                              '22°C',
-                              style: GoogleFonts.lato(
-                                fontSize: 22,
-                                color: Colors.blue,
-                              ),
-                            ),
-                            SizedBox(width: 20),
+                            index < forecastData.length - 1
+                                ? const Divider(
+                                    thickness: 1,
+                                    color: Color.fromRGBO(231, 231, 231, 1),
+                                  )
+                                : const SizedBox(),
                           ],
                         ),
-                        Divider(
-                          thickness: 1,
-                          color: Color.fromRGBO(231, 231, 231, 1),
-                        ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                 ),
               ],
@@ -87,5 +90,11 @@ class MyDraggableScrollable extends StatelessWidget {
         },
       ),
     );
+  }
+
+  String _convertToDayOfWeek(String date) {
+    final parsedDate = DateTime.parse(date);
+    final formatter = DateFormat.EEEE('pt_BR'); // Formato longo do dia
+    return formatter.format(parsedDate); // Ex: Segunda-feira
   }
 }
